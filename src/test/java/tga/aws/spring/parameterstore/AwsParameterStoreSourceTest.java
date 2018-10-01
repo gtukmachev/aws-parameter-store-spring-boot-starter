e69f7c30abf083c5019436dfe4fa6860550ac249
Grigory <gtukmachev@gmail.com>
2018-10-01 12:50:26 +0700

    AwsParameterStoreSourceTest restored

--- src/test/java/tga/aws/spring/parameterstore/AwsParameterStoreSourceTest.java
+++ src/test/java/tga/aws/spring/parameterstore/AwsParameterStoreSourceTest.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.simplesystemsmanagement.model.GetParameterResult;
 import com.amazonaws.services.simplesystemsmanagement.model.Parameter;
 import com.amazonaws.services.simplesystemsmanagement.model.ParameterNotFoundException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -19,9 +18,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-@Ignore
-public class AwsParameterStoreSourceTest
-{
+public class AwsParameterStoreSourceTest {
     private static final String VALID_PROPERTY_NAME = "awesomeproperty";
     private static final String VALID_PROPERTY_VALUE = "awesomepropertyVALUE";
 
@@ -33,15 +30,15 @@ public class AwsParameterStoreSourceTest
     private AwsParameterStoreSource parameterStoreSource;
 
     @Before
-    public void setUp()
-    {
+    public void setUp() {
         parameterStoreSource = new AwsParameterStoreSource(ssmClientMock);
     }
 
     @Test
-    public void testGetProperty()
-    {
-        when(ssmClientMock.getParameter(getParameterRequest(VALID_PROPERTY_NAME))).thenReturn(new GetParameterResult().withParameter(new Parameter().withValue(VALID_PROPERTY_VALUE)));
+    public void testGetProperty() {
+        when(ssmClientMock.getParameter(request(VALID_PROPERTY_NAME)))
+                .thenReturn(new GetParameterResult()
+                        .withParameter(new Parameter().withValue(VALID_PROPERTY_VALUE)));
 
         Object value = parameterStoreSource.getProperty(VALID_PROPERTY_NAME);
 
@@ -49,9 +46,9 @@ public class AwsParameterStoreSourceTest
     }
 
     @Test
-    public void testGetPropertyWhenNotFoundReturnsNull()
-    {
-        when(ssmClientMock.getParameter(getParameterRequest(INVALID_PROPERTY_NAME))).thenThrow(new ParameterNotFoundException(""));
+    public void testGetPropertyWhenNotFoundReturnsNull() {
+        when(ssmClientMock.getParameter(request(INVALID_PROPERTY_NAME)))
+                .thenThrow(new ParameterNotFoundException(""));
 
         Object value = parameterStoreSource.getProperty(INVALID_PROPERTY_NAME);
 
@@ -59,15 +56,14 @@ public class AwsParameterStoreSourceTest
     }
 
     @Test(expected = AwsParameterStoreConnectorException.class)
-    public void shouldThrowOnUnexpectedExceptionAccessingParameterStore()
-    {
-        when(ssmClientMock.getParameter(getParameterRequest(VALID_PROPERTY_NAME))).thenThrow(new RuntimeException());
+    public void shouldThrowOnUnexpectedExceptionAccessingParameterStore() {
+        when(ssmClientMock.getParameter(request(VALID_PROPERTY_NAME)))
+                .thenThrow(new RuntimeException());
 
         parameterStoreSource.getProperty(VALID_PROPERTY_NAME);
     }
 
-    private GetParameterRequest getParameterRequest(String parameterName)
-    {
+    private GetParameterRequest request(String parameterName) {
         return new GetParameterRequest().withName(parameterName).withWithDecryption(true);
     }
 }
