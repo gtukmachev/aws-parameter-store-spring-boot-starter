47a72b61478c8a7204d6bbdc23362bb04d359bc2
Grigory <gtukmachev@gmail.com>
2018-10-09 14:26:07 +0700

    tests skeleton

---
+++ src/test/java/tga/aws/spring/parameterstore/AWSParameterStoreClientBuilderTests.java
@@ -0,0 +1,25 @@
+package tga.aws.spring.parameterstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Created by grigory@clearscale.net on 10/9/2018.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AWSParameterStoreClientBuilderTests {
+
+    @Test
+    public void clientShouldBeNullIfAnyErrorRisedInAwsBackgroundLibrary() {
+        //todo: implement
+        // it's imposible to implement it now, becouse of we have to mock a static method
+    }
+
+    @Test
+    public void aLoggingMessageWithExceptionClassAndMessageShouldBeWroteIntoStdOutStream() {
+        //todo: implement
+        // it's imposible to implement it now, becouse of we have to mock a static method
+    }
+
+}
